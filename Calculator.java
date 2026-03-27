23b3faa3f2f9dcb1ccd656c636e102e115168726
elinpelin01 <elinpiho1@gmail.com>
2026-03-27 10:50:55 +0100

    Bug: make sub() wrong on purpose

--- Calculator.java
+++ Calculator.java
@@ -8,7 +8,9 @@ public class Calculator {
     }
 
     /** Returns the difference of a and b. */
-    public static double sub(double a, double b) { return a -b; }
+    public static double sub(double a, double b) {
+        return a + b;
+    }
 
     /** Returns the product of a and b. */
     public static double mul(double a, double b){
