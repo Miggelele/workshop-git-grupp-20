f855d5c5bdf019361d419d90091942f35edde991
BellMAU <isabellpersson05@gmail.com>
2026-03-27 10:15:28 +0100

    Menu: include Subtract

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 public class App {
     static void run() {
         System.out.println("2) Subtract \n0) Exit");
+        System.out.println("1) Add\n2) Subtract \n0) Exit");
         System.out.println("Your choice: ");
 
         String choice = new java.util.Scanner(System.in).nextLine();
