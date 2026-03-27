7993b9d6c6d394869acc41d76bd2133ad43fccf1
elinpelin01 <elinpiho1@gmail.com>
2026-03-27 10:05:34 +0100

    Use Calculator.add() placeholder in App

--- App.java
+++ App.java
@@ -4,5 +4,9 @@ public class App {
         System.out.println("Your choice: ");
 
         String choice = new java.util.Scanner(System.in).nextLine();
+
+        if ("1"-equals(choice)) {
+            System.out.println(Calculator.add(0,0));
+        }
     }
 }
