2da6727fb0ab14e9259c8f12f5721efe1b9901f1
Frida <frida.2181@gmail.com>
2026-03-27 10:18:43 +0100

    Menu: include Divide

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 public class App {
     static void run() {
-        System.out.println("2) Subtract \n0) Exit");
-        System.out.println("1) Add\n2) Subtract \n0) Exit");
+        System.out.println("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit");
         System.out.println("Your choice: ");
 
         String choice = new java.util.Scanner(System.in).nextLine();
