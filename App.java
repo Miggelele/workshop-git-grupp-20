e03939089e48d2655c74209f0f187f85322dd7a7
Miggelele <mikael.szalai@gmail.com>
2026-03-27 10:26:02 +0100

    Read operand a

--- App.java
+++ App.java
@@ -9,6 +9,7 @@ public class App {
 
         if ("1".equals(choice)) {
             System.out.println("TODO Add");
+            double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         }
     }
 }
