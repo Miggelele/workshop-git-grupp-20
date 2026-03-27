283fda591c721002ab31debbc20ba8f059844e91
Miggelele <mikael.szalai@gmail.com>
2026-03-27 09:41:23 +0100

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
     static void run() {
+<<<<<<< HEAD
         System.out.println("1) Subtract \n0) Exit");
         System.out.println("Your choice: ");
+=======
         System.out.println("1) Add\n0) Exit");
         System.out.print("Choice: ");
+>>>>>>> 7f0f34af104f4f55aef653e3902183a28f45ec16
         String choice = new java.util.Scanner(System.in).nextLine();
     }
 }
